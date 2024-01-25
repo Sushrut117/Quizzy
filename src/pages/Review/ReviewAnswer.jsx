5a4b550ca0082e41b1569a401aaa0dd63154267d
Sushrut Sharma <sharmasushrut7@gmail.com>
2024-01-25 09:01:04 +0530

    Update ReviewAnswer.jsx

--- src/pages/Review/ReviewAnswer.jsx
+++ src/pages/Review/ReviewAnswer.jsx
@@ -1,24 +1,27 @@
-import React, { useContext } from 'react'
-import quizContext from '../../context/quizContext'
-import ReviewAnswerBox from './../../components/ReviewAnswerBox/ReviewAnswerBox'
+import React, { useContext } from 'react';
+import quizContext from '../../context/quizContext';
+import ReviewAnswerBox from './../../components/ReviewAnswerBox/ReviewAnswerBox';
+
 const ReviewAnswer = () => {
-    const context = useContext(quizContext)
-    const { answerList } = context
+  const context = useContext(quizContext);
+  const { answerList } = context;
 
+  console.log(answerList)
+  return (
+    <>
+      {answerList.map((item, index) => (
+        <ReviewAnswerBox
+          key={`${item.question}_${index}`} // Combine question and index
+          myAnswer={item.myAnswer}
+          rightAnswer={item.rightAnswer}
+          question={item.question}
+          options={item.options}
+          category={item.category}
+          num={index + 1}
+        />
+      ))}
+    </>
+  );
+};
 
-    return (
-        <>
-            {
-                answerList.map((index) => {
-                    const { question, options, category, myAnswer, rightAnswer } = index
-
-                    return <ReviewAnswerBox key={question} myAnswer={myAnswer} rightAnswer={rightAnswer} question={question} options={options} category={category} num={answerList.indexOf(index) + 1} />
-                })
-            }
-        </>
-    )
-}
-
-export default ReviewAnswer
-
-
+export default ReviewAnswer;
